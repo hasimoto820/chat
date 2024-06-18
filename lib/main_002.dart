d80b9afdb08f6e2c88fab641c4ba601a6e758fba
hasimoto820 <hashimotokoji9999@gmail.com>
2024-06-18 17:27:44 +0900

    first

---
+++ lib/main_002.dart
@@ -0,0 +1,22 @@
+import 'package:chat/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  // main 関数でも async が使えます
+  WidgetsFlutterBinding.ensureInitialized(); // runApp 前に何かを実行したいときはこれが必要です。
+  await Firebase.initializeApp(
+    // これが Firebase の初期化処理です。
+    options: DefaultFirebaseOptions.android,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
